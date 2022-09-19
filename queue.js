c42e1a9c5ab7a2a440e2d84e6150cf0ed406fc01
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-19 15:15:20 -0700

    queue is done

--- queue.js
+++ queue.js
@@ -20,25 +20,54 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    let newNode = new Node(val);
+    if (this.first === null) this.first = newNode;
+    if (this.last !== null) this.last.next = newNode;
+    this.last = newNode;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) throw new Error;
+
+    let poppedVal = this.first.val;
+
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return poppedVal;
+    }
+
+    let current = this.first;
+
+    poppedVal = current.val;
+    this.first = current.next;
+    this.size--;
+
+
+
+
+    return poppedVal;
+
+
 
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    return this.first.val
 
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
+    return this.size === 0 ? true : false
 
   }
 }
